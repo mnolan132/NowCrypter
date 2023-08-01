4a43d5b320b7ffac2907c28ef22230c1cd1c2c52
Matthew Nolan <mnolan132@gmail.com>
2023-08-01 14:42:27 +1200

    logo file path bug

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,11 @@
 import { Box, Image } from "@chakra-ui/react";
+import logo from "../assets/logo.png";
 
 export const Nav = () => {
   return (
     <>
       <Box display={"flex"} alignItems={"center"} py={"10px"}>
-        <Image src="../src/assets/logo.png" w={"250px"}></Image>
+        <Image src={logo} w={"250px"}></Image>
       </Box>
     </>
   );
